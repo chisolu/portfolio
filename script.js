10001b72097305035f0ff326cea931aa4043ed66
Ucheonye C . H Umeotawara <uchisolu@gmail.com>
2025-06-26 16:58:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const dropdownEL = document.getElementById('dropdown-menu');
+
+// function   menuFun(){
+//     dropdownEL.style.display='flex'
+// }
+
+function   menuFun(){
+    dropdownEL.classList.toggle('menu-right-open');
+}
